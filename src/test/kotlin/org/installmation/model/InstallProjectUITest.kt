195c9ae53a616e65d2e76fe6d1927a4b954befd2
SergeMerzliakov <serge.merzliakov@qantasloyalty.com>
2019-12-15 12:10:09 +1100

    Added tests for saving and restoring project components from all UI tabs and controllers.

--- src/test/kotlin/org/installmation/model/InstallProjectUITest.kt
+++ src/test/kotlin/org/installmation/model/InstallProjectUITest.kt
@@ -27,6 +27,7 @@ import org.installmation.core.RunningAsTestEvent
 import org.installmation.io.ApplicationJsonReader
 import org.installmation.javafx.test.FXID
 import org.installmation.javafx.test.JavaFxTest
+import org.installmation.model.binary.JDK
 import org.junit.AfterClass
 import org.junit.Test
 import java.io.File
@@ -60,17 +61,10 @@ class InstallProjectUITest : JavaFxTest() {
    @Test
    fun shouldCreateAndSaveNewEmptyProject() {
       val projectName = "project1"
-      clickOn(FXID.MENU_PROJECT).clickOn(FXID.MENUITEM_NEW_PROJECT)
-      writeTextField(FXID.TEXT_SINGLEVAL_DLG_ITEM_VALUE, projectName)
-      clickOn(FXID.BUTTON_SINGLEVAL_DLG_SAVE)
-      clickOn(FXID.MENU_PROJECT).clickOn(FXID.MENUITEM_SAVE_PROJECT)
+      createProject(projectName)
 
-      // now check saved project exists
-      val projectFile = File(File(BASE_CONFIG_DIR, Constant.PROJECT_DIR), "$projectName.json")
-      assertThat(projectFile).exists()
-      val reader = ApplicationJsonReader<InstallProject>(InstallProject::class, projectFile, JsonParserFactory.configurationParser())
-      val project = reader.load()
-      
+      val project = loadCurrentProject(projectName)
+
       assertThat(project.name).isEqualTo(projectName)
       assertThat(project.installerType).isNotEmpty() //OS specific default values
       assertThat(project.version).isEmpty()
@@ -94,33 +88,24 @@ class InstallProjectUITest : JavaFxTest() {
       val originalProjectName = "otherName"
       val version = "1"
       val copyrightMessage = "copyright"
-      clickOn(FXID.MENU_PROJECT).clickOn(FXID.MENUITEM_NEW_PROJECT)
-      writeTextField(FXID.TEXT_SINGLEVAL_DLG_ITEM_VALUE, originalProjectName)
-      clickOn(FXID.BUTTON_SINGLEVAL_DLG_SAVE)
-      clickOn(FXID.MENU_PROJECT).clickOn(FXID.MENUITEM_SAVE_PROJECT)
-      
-      // project info properties
-      clickOn(FXID.TAB_INFO)
 
+      createProject(originalProjectName)
+
+      // set single tab properties and save to disk
+      clickOn(FXID.TAB_INFO)
       writeTextField(FXID.TEXT_PROJECT_NAME, projectName)
       writeTextField(FXID.TEXT_APP_VERSION, version)
       writeTextField(FXID.TEXT_COPYRIGHT, copyrightMessage)
-
       val installer = selectComboByIndex<String>(FXID.COMBO_INSTALLER_TYPE, 0)
-      clickOn(FXID.MENU_PROJECT).clickOn(FXID.MENUITEM_SAVE_PROJECT)
-      
-      // now check saved project exists
-      val projectFile = File(File(BASE_CONFIG_DIR, Constant.PROJECT_DIR), "$projectName.json")
-      assertThat(projectFile).exists()
-      val reader = ApplicationJsonReader<InstallProject>(InstallProject::class, projectFile, JsonParserFactory.configurationParser())
-      val project = reader.load()
+      saveCurrentProject()
+
+      val project = loadCurrentProject(projectName)
 
       assertThat(project.name).isEqualTo(projectName)
       assertThat(project.installerType).isEqualTo(installer) //OS specific default values
       assertThat(project.version).isEqualTo(version)
       assertThat(project.copyright).isEqualTo(copyrightMessage)
 
-      assertThat(project.imageBuildDirectory).isNull()
       assertThat(project.imageBuildDirectory).isNull()
       assertThat(project.installerDirectory).isNull()
       assertThat(project.inputDirectory).isNull()
@@ -132,5 +117,178 @@ class InstallProjectUITest : JavaFxTest() {
       assertThat(project.javaFXLib).isNull()
       assertThat(project.javaFXMods).isNull()
       assertThat(project.jpackageJDK).isNull()
+   }
+
+   @Test
+   fun shouldCreateAndSaveProjectInstallLocation() {
+      val projectName = "locate"
+      val inputDir = "/input"
+      val imageBuildDir = "/image"
+      val installerDir = "/installer"
+
+      createProject(projectName)
+
+      // set single tab properties and save to disk
+      clickOn(FXID.TAB_INSTALL)
+      writeTextField(FXID.TEXT_INPUT_DIR, inputDir)
+      writeTextField(FXID.TEXT_IMAGE_BUILD_DIR, imageBuildDir)
+      writeTextField(FXID.TEXT_INSTALLER_DIR, installerDir)
+      saveCurrentProject()
+
+      val project = loadCurrentProject(projectName)
+
+      assertThat(project.name).isEqualTo(projectName)
+      assertThat(project.installerType).isNotEmpty() //OS specific default values
+      assertThat(project.version).isNullOrEmpty()
+      assertThat(project.copyright).isNullOrEmpty()
+
+      assertThat(project.inputDirectory?.path).isEqualTo(inputDir)
+      assertThat(project.imageBuildDirectory?.path).isEqualTo(imageBuildDir)
+      assertThat(project.installerDirectory?.path).isEqualTo(installerDir)
+      assertThat(project.installJDK).isNull()
+      assertThat(project.mainClass).isNull()
+      assertThat(project.mainJar).isNull()
+      assertThat(project.classPath).isEmpty()
+      assertThat(project.modulePath).isEmpty()
+      assertThat(project.javaFXLib).isNull()
+      assertThat(project.javaFXMods).isNull()
+      assertThat(project.jpackageJDK).isNull()
+   }
+
+   @Test
+   fun shouldCreateAndSaveProjectJavaBinaries() {
+      val projectName = "binaries"
+      val jpackageJDK = TestJDKFactory.create("package-jdk")
+      val installJDK = TestJDKFactory.create("install-jdk")
+      val moduleDir = NamedDirectory("module1", File("/module1"))
+      val jmodDir = NamedDirectory("jmod1", File("/jmod1"))
+
+      createProject(projectName)
+
+      populateCombo(FXID.COMBO_JPACKAGE, jpackageJDK)
+      populateCombo(FXID.COMBO_INSTALL_JDK, installJDK)
+      populateCombo(FXID.COMBO_MODULE_LIB, moduleDir)
+      populateCombo(FXID.COMBO_MODULE_JMOD, jmodDir)
+
+      // set single tab properties and save to disk
+      clickOn(FXID.TAB_BINARIES)
+      selectComboByIndex<JDK>(FXID.COMBO_JPACKAGE, 0)
+      selectComboByIndex<JDK>(FXID.COMBO_INSTALL_JDK, 0)
+      selectComboByIndex<NamedDirectory>(FXID.COMBO_MODULE_LIB, 0)
+      selectComboByIndex<NamedDirectory>(FXID.COMBO_MODULE_JMOD, 0)
+
+      saveCurrentProject()
+
+      val project = loadCurrentProject(projectName)
+
+      assertThat(project.name).isEqualTo(projectName)
+      assertThat(project.installerType).isNotEmpty() //OS specific default values
+      assertThat(project.version).isNullOrEmpty()
+      assertThat(project.copyright).isNullOrEmpty()
+
+      assertThat(project.installJDK).isEqualTo(installJDK)
+      assertThat(project.jpackageJDK).isEqualTo(jpackageJDK)
+      assertThat(project.javaFXLib).isEqualTo(moduleDir)
+      assertThat(project.javaFXMods).isEqualTo(jmodDir)
+      assertThat(project.modulePath).hasSize(1)
+
+      assertThat(project.inputDirectory).isNull()
+      assertThat(project.imageBuildDirectory).isNull()
+      assertThat(project.installerDirectory).isNull()
+      assertThat(project.mainClass).isNull()
+      assertThat(project.mainJar).isNull()
+      assertThat(project.classPath).isEmpty()
+   }
+
+   @Test
+   fun shouldCreateAndSaveProjectApplicationExecutables() {
+      val projectName = "exec"
+      val mainJar = "/usr/main.jar"
+      val mainClass = "org.Main"
+
+      createProject(projectName)
+
+      // set single tab properties and save to disk
+      clickOn(FXID.TAB_EXECUTABLE)
+      writeTextField(FXID.TEXT_MAIN_JAR, mainJar)
+      writeTextField(FXID.TEXT_MAIN_CLASS, mainClass)
+
+      saveCurrentProject()
+
+      val project = loadCurrentProject(projectName)
+
+      assertThat(project.name).isEqualTo(projectName)
+      assertThat(project.installerType).isNotEmpty() //OS specific default values
+      assertThat(project.version).isNullOrEmpty()
+      assertThat(project.copyright).isNullOrEmpty()
+
+      assertThat(project.mainClass).isEqualTo(mainClass)
+      assertThat(project.mainJar?.path).isEqualTo(mainJar)
+
+      assertThat(project.installJDK).isNull()
+      assertThat(project.jpackageJDK).isNull()
+      assertThat(project.javaFXLib).isNull()
+      assertThat(project.javaFXMods).isNull()
+      assertThat(project.modulePath).isEmpty()
+
+      assertThat(project.inputDirectory).isNull()
+      assertThat(project.imageBuildDirectory).isNull()
+      assertThat(project.installerDirectory).isNull()
+      assertThat(project.classPath).isEmpty()
+   }
+
+   @Test
+   fun shouldCreateAndSaveProjectDependencies() {
+      val projectName = "deps"
+      val classPathItem = "/usr/lib"
+
+      createProject(projectName)
+
+      // set single tab properties and save to disk
+      clickOn(FXID.TAB_DEPENDENCIES)
+      populateListView(FXID.LISTVIEW_CLASSPATH, classPathItem)
+
+      saveCurrentProject()
+
+      val project = loadCurrentProject(projectName)
+
+      assertThat(project.name).isEqualTo(projectName)
+      assertThat(project.installerType).isNotEmpty() //OS specific default values
+      assertThat(project.version).isNullOrEmpty()
+      assertThat(project.copyright).isNullOrEmpty()
+
+      assertThat(project.classPath).hasSize(1)
+      assertThat(project.classPath).contains(File(classPathItem))
+
+      assertThat(project.mainClass).isNull()
+      assertThat(project.mainJar).isNull()
+
+      assertThat(project.installJDK).isNull()
+      assertThat(project.jpackageJDK).isNull()
+      assertThat(project.javaFXLib).isNull()
+      assertThat(project.javaFXMods).isNull()
+      assertThat(project.modulePath).isEmpty()
+
+      assertThat(project.inputDirectory).isNull()
+      assertThat(project.imageBuildDirectory).isNull()
+      assertThat(project.installerDirectory).isNull()
+   }
+
+   private fun saveCurrentProject() {
+      clickOn(FXID.MENU_PROJECT).clickOn(FXID.MENUITEM_SAVE_PROJECT)
+   }
+
+   private fun createProject(projectName: String) {
+      clickOn(FXID.MENU_PROJECT).clickOn(FXID.MENUITEM_NEW_PROJECT)
+      writeTextField(FXID.TEXT_SINGLEVAL_DLG_ITEM_VALUE, projectName)
+      clickOn(FXID.BUTTON_SINGLEVAL_DLG_SAVE)
+      saveCurrentProject()
+   }
+
+   private fun loadCurrentProject(projectName: String): InstallProject {
+      val projectFile = File(File(BASE_CONFIG_DIR, Constant.PROJECT_DIR), "$projectName.json")
+      assertThat(projectFile).exists()
+      val reader = ApplicationJsonReader<InstallProject>(InstallProject::class, projectFile, JsonParserFactory.configurationParser())
+      return reader.load()
    }
 }
