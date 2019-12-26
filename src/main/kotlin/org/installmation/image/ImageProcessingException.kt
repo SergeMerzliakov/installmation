f03ecd3222bb96dca5d2ee3aa357c40b44802706
SergeMerzliakov <serge.merzliakov@qantasloyalty.com>
2019-12-26 11:49:32 +1100

    Exception thrown when processing images - application logos and splash screens

---
+++ src/main/kotlin/org/installmation/image/ImageProcessingException.kt
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2019 Serge Merzliakov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.installmation.image
+
+/**
+ * Thrown for all image handling errors
+ */
+class ImageProcessingException : Exception {
+   constructor() : super()
+   constructor(message: String?) : super(message)
+   constructor(message: String?, cause: Throwable?) : super(message, cause)
+}
